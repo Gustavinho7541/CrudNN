13a9c7555652288e798b213672faeff97dbc65d5
Gustavo <gupsjales@gmail.com>
2026-05-21 08:02:12 -0300

    Lucas Pro

---
+++ Infrastructure/InvariantDecimalModelBinderProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+namespace CrudNovoNN.Infrastructure
+{
+    public class InvariantDecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            var t = context.Metadata.UnderlyingOrModelType;
+            if (t == typeof(decimal) || t == typeof(decimal?))
+                return new InvariantDecimalModelBinder();
+            return null;
+        }
+    }
+}
