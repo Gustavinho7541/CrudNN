1974da422a1ccdbe512cb02e819d6493930375a7
Gustavo <gupsjales@gmail.com>
2026-06-05 15:40:09 -0300

    Correção do Sistema

--- Program.cs
+++ Program.cs
@@ -2,38 +2,66 @@ using CrudNovoNN.Data;
 using CrudNovoNN.Infrastructure;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Globalization;
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
+
+// MVC
 builder.Services.AddControllersWithViews(options =>
 {
-    // nosso binder vem primeiro
     options.ModelBinderProviders.Insert(0, new InvariantDecimalModelBinderProvider());
 });
-var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseNpgsql(connStr));
-// (Opcional) mapear DateTime antigo do Npgsql se você migrar projetos antigos:
-// AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// CONEXÃO FORÇADA
+var connStr = "Host=localhost;Port=5433;Database=CRUDNN;Username=postgres;Password=123456;Include Error Detail=true";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connStr));
+
+// Cultura pt-BR
 var supportedCultures = new[] { new CultureInfo("pt-BR") };
-builder.Services.Configure<RequestLocalizationOptions>(opts =>
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
-    opts.DefaultRequestCulture = new RequestCulture("pt-BR");
-    opts.SupportedCultures = supportedCultures;
-    opts.SupportedUICultures = supportedCultures;
+    options.DefaultRequestCulture = new RequestCulture("pt-BR");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
 });
+
 var app = builder.Build();
+
+// CRIA O BANCO/TABELAS AUTOMATICAMENTE
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    try
+    {
+        db.Database.EnsureCreated();
+        Console.WriteLine("Banco conectado com sucesso.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Erro ao conectar no banco:");
+        Console.WriteLine(ex.Message);
+    }
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
